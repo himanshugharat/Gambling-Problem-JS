16ca456c0a8cdd46fef6f867272ed74465818aa7
himanshu <himanshugharat44@gmail.com>
2020-09-14 06:25:29 -0500

    ADDED: the gambler.js and the description comments

---
+++ gamble.js
@@ -0,0 +1,9 @@
+/*
+ *Execution: default node cmd>node gambler.js
+ *Purpose  : To simulate the gamblers month earnings
+ *@files   : gambler.js
+ *@overview: Gambler simulation of his gambling
+ *@author  : Himanshu Gharat
+ *@verson  : 1.0
+ *@since   : 14-09-2020
+ */
